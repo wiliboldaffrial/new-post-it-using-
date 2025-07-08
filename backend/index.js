be60feae2ba830e2dceef4d4eca7ed2da3d99874
qemal <gembol1419@gmail.com>
2025-07-08 13:55:39 +0700

    update isPinned

--- backend/index.js
+++ backend/index.js
@@ -222,6 +222,36 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
   }
 });
 
+// Update isPinned
+app.put("/update-note-pinned/:noteId", authenticateToken, async (res, req) => {
+  const noteId = req.params.noteId;
+  const { isPinned } = req.body;
+  const { user } = req.user;
+
+  try {
+      const note = await note.findOne({ _id: noteId, userId: user._id });
+
+      if (!note) {
+          return res.status(404).json({error: true, message:"Note note found"});
+      }
+
+      note.isPinned = isPinned;
+
+      await note.save();
+
+      return res.json({
+          error: false,
+          note,
+          message: "Note updated successfully",
+      });
+  } catch (error) {
+      return res.status(500).json({
+          error: true,
+          message: "interlnal Server Error",
+      });
+  }
+});
+
 app.listen(8000);
 
 module.exports = app;
