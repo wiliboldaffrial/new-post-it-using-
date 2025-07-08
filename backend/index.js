f8fe9f0b0d8ecaeee4d0c4aedb8e3677879db9f6
qemal <gembol1419@gmail.com>
2025-07-08 11:59:26 +0700

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,43 @@ app.get("/", (req, res) =>{
     res.json({data: "Hello"});
 });
 
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ message: "Email is required" });
+    }
+
+    if (!password) {
+        return res.status(400).json({ message: "Password is required"})
+    }
+
+    const userInfo = await UserActivation.findOne({ email: email});
+
+    if (!userInfo) {
+        return res.status(400).json({ message: "User not found" });
+    }
+
+    if (userInfo.email == email && userInfo.password == password) {
+        const user = { user: userInfo };
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "36000m",
+        });
+
+        return res.json({
+            error: false,
+            message: "Login Successful",
+            email,
+            accessToken,
+        });
+    } else {
+        return res.status(400).json({
+            error: true,
+            message: "Invalid Credentials",
+        });
+    }
+});
+
 app.listen(8000);
 
 module.exports = app;
