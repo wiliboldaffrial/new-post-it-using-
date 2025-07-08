75e9ae2a34221ac7828145c8f6ad0744a66d90ac
Wilibold-Affrial <wilibold.g@gmail.com>
2025-07-08 13:35:46 +0700

    Added get all notes in index.js

--- backend/index.js
+++ backend/index.js
@@ -185,6 +185,26 @@ app.post("/edit-note/:noteId", authenticateToken, async (req, res) => {
     }
 })
 
+app.post("/get-all-notes", authenticateToken, async (req, res) => {
+    const { user } = req.user;
+
+    try{
+        const notes = await Note.find({ userId: user._id})
+        .sort({ isPinned: -1})
+
+        return res.json({
+            error: false,
+            notes,
+            message: "All notes retrieved successfully",
+        });
+    } catch (error){
+        return res.status(500). json({
+            error: true,
+            message: "Internal Server Error",
+        });
+    }
+})
+
 app.listen(8000);
 
 module.exports = app;
