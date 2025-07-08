b2aa703e7a8bcb0673fb2005b9eb9c17af386ed9
Wilibold-Affrial <wilibold.g@gmail.com>
2025-07-08 13:27:45 +0700

    Added edit note on index js backend

--- backend/index.js
+++ backend/index.js
@@ -146,6 +146,45 @@ app.post("/add-note", authenticateToken, async (req, res) => {
         });
     }
 });
+
+app.post("/edit-note/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const { user } = req.user;
+
+    if(!title && !content && !tags){
+        return res
+            .status(400)
+            .json({ error: true, message: "No changes provided" });
+    }
+
+    try {
+        const note = await note.findOne({ _id: noteId, userId: user._id });
+
+        if (!note){
+            return res.status(404).json({error: true, message:"Note note found"});
+        }
+
+        if (title) note.title = title;
+        if (content) note.content = content;
+        if (tags) note.tags = tags;
+        if (isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated successfully",
+        });
+    } catch (error){
+        return res.status(500).json({
+            error: true,
+            message: "interlnal Server Error",
+        });
+    }
+})
+
 app.listen(8000);
 
 module.exports = app;
